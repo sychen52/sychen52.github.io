1b41bc894b887663cd8c41bb57ab00724d2b5089
sychen52 <sychen52@gmail.com>
2021-11-17 20:51:14 -0800

    test org-html-postamble-format

--- build-site.el
+++ build-site.el
@@ -24,16 +24,18 @@
       org-html-head-include-default-style nil ;; Use our own styles
       org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
 
-(setq org-html-postamble-format '(("en"
-                                   "<p><script src='https://utteranc.es/client.js'
-          repo='sychen52/sychen52.github.io'
-          issue-term='title'
-          label='Comment'
-          theme='github-dark'
-          crossorigin='anonymous'
-          async>
-</script></p>
-")))
+(setcar (cdar org-html-postamble-format)
+	(concat (cadar org-html-postamble-format)
+		"<script src='https://utteranc.es/client.js'
+	repo='sychen52/sychen52.github.io'
+	issue-term='pathname'
+	theme='github-dark'
+	crossorigin='anonymous'
+	async>
+</script>"))
+
+
+(print org-html-postamble-format)
 
 (setq org-publish-project-alist
       (list
