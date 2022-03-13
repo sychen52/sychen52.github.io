f0d3f36426cd640d08474091d341d45ab7996a67
sychen52 <sychen52@gmail.com>
2022-03-13 15:19:27 -0700

    better css

--- build-site.el
+++ build-site.el
@@ -22,7 +22,8 @@
       org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
-      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
+      org-html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"https://gongzhitaao.org/orgcss/org.css\"/>")
+
 
 (setq org-publish-project-alist
       '(("org" :components ("org-site:main" "org-static"))
